43b785bd9977d6492d9c6bf3fae2751a5ea46c4c
jymmak <henryfrancisco_fernando@hotmail.com>
2017-12-30 16:35:08 -0500

    modal colorao precio

--- js/app.js
+++ js/app.js
@@ -1,5 +1,46 @@
-$(document).ready(function(){
-  
-/**Splash*/
- $('.title').delay(2000).fadeOut("slow");
+$(document).ready(function () {
+
+  /**Splash*/
+  $('.title').delay(2000).fadeOut("slow");
+
+  $(function () {
+    $('#preciodiv').hide();
+    $('#alphord').hide();
+    $('#valdiv').hide();
+    $('#filtrores').change(function () {
+      if ($('#filtrores').val() == 'price') {
+        $('#preciodiv').show();
+        $('.img-responsive').hide();
+      } else {
+        $('#preciodiv').hide();
+      }
+      if ($('#filtrores').val() == 'locales abiertos') {
+        $('#abiertodiv').show();
+        $('.img-responsive').hide();
+      } else {
+        $('#abiertodiv').hide();
+      }
+      if ($('#filtrores').val() == 'valoracion') {
+        $('#valdiv').show();
+        $('.img-responsive').hide();
+      } else {
+        $('#valdiv').hide();
+      }
+      if ($('#filtrores').val() == 'orden alfabetico') {
+        $('#alphord').show();
+        $('.img-responsive').hide();
+      } else {
+        $('#alphord').hide();
+      }XMLDocument
+    });
+  });
+});
+
+$('.elcolorao').on({
+  'mouseover': function () {
+    var src = ($(this).attr('src') === 'assets/images/elcolorao2.png')
+      ? 'assets/images/elcolorao.png'
+      : 'assets/images/elcolorao2.png';
+    $(this).attr('src', src);
+  }
 });
